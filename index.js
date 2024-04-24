88165651c672a891da4d1cf78cdb0b1438205d70
0xfzs <arpit.srivastava@rangeprotocol.com>
2024-04-24 18:21:36 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const { ethers } = require('ethers');
 
 const axios = require('axios'); // Axios is used to make HTTP requests
+const  REBEL_ABI  = require('./REBEL_ABI.json');
 require('dotenv').config();
 
 // Replace with your Ethereum RPC URL and Slack Webhook URL
 const provider = new ethers.providers.JsonRpcProvider(process.env.NOLLIE_RPC_URL);
 const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+const NFT_CONTRACT_ADDRESS = '0xD926979723Ee65B17B3a685E8C3219864A4ACF2D';
+
+const contract = new ethers.Contract(NFT_CONTRACT_ADDRESS, REBEL_ABI, provider);
+let lastCheckedId;
 
 async function sendToSlack(message) {
     try {
@@ -42,7 +47,6 @@ async function checkBlockAndExplorer() {
 async function checkBlockOnExplorer(blockNumber) {
     try {
         const response = await axios.get(process.env.BLOCKEXPLORER_URL.replace('{blockNumber}', blockNumber));
-        console.log(response.data)
         if (response.data && response.data.pageProps && response.data.pageProps.query.height_or_hash === blockNumber.toString()) {
             console.log(`Block number ${blockNumber} is present on the explorer.`);
         } else {
@@ -77,20 +81,47 @@ async function checkAddressBalances() {
 }
 
 
+async function checkRebelsCurrentId() {
+    try {
+        const currentId = await contract.rebelsCurrentId();
+        console.log('lastCheckedId', lastCheckedId, 'currentCheckedId', currentId.toString());
+        if (lastCheckedId !== undefined && lastCheckedId === currentId.toString()) {
+            await sendToSlack('Alert: rebelsCurrentId has not changed in the last 15 minutes i.e. No NEW MINTS .  <!subteam^S06TSCPGBDW|engineering> ');
+        }
+        lastCheckedId = currentId.toString();
+    } catch (error) {
+        await sendToSlack(`Error retrieving rebelsCurrentId from the contract  <!subteam^S06TSCPGBDW|engineering> : ${error.message}`);
+    }
+}
+
+
 function scheduleChecks() {
     try {
         // Send an initial startup message
         sendToSlack('I am Up <!subteam^S06TSCPGBDW|engineering>').catch(console.error);
 
-        // Schedule the recurring checks
+        // Schedule the recurring checks for block and address balances every 30 seconds
         setInterval(async () => {
             try {
                 await checkBlockAndExplorer();
                 await checkAddressBalances();
+                await checkRebelsCurrentId();
             } catch (error) {
                 sendToSlack(`Some error occurred <!subteam^S06TSCPGBDW|engineering> ${error.message}`).catch(console.error);
             }
         }, 30 * 1000);
+
+        // Schedule the check for rebelsCurrentId every 15 minutes
+        setInterval(() => {
+            try {
+                checkRebelsCurrentId().catch(error => {
+                    console.error('Failed to check rebelsCurrentId:', error);
+                    sendToSlack(`Error occurred while checking rebelsCurrentId: ${error.message}`).catch(console.error);
+                });
+            } catch (error) {
+                sendToSlack(`Some error occurred during the rebelsCurrentId check: ${error.message}`).catch(console.error);
+            }
+        }, 15 * 60 * 1000); // 900000 milliseconds
 
         // Schedule a daily Good Morning message
         const sendDailyGreeting = () => {
@@ -112,7 +143,6 @@ function scheduleChecks() {
         sendToSlack(`Some error occurred <!subteam^S06TSCPGBDW|engineering> ${error.message}`).catch(console.error);
     }
 }
-
 
 scheduleChecks();
 
